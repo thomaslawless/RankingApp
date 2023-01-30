9eb77805affaa0738dd22d70f1d0a01f07e82c45
thomaslawless <lawlessptom@gmail.com>
2023-01-30 12:20:08 -0500

    rankingitems.js created

---
+++ RankingApp/ClientApp/src/components/RankItems.js
@@ -0,0 +1,29 @@
+﻿import React, { useState, useEffect } from 'react';
+
+const RankItems = () => {
+
+    const [items, setItems] = useState([]);
+    // items retrieved from server side code
+    // set items for react component
+
+    const dataType = 1;
+
+    useEffect(() => {
+        fetch(`item/${dataType}`)
+            .then((results) => {
+                return results.json();
+            })
+            .then(data => {
+                setItems(data);
+            })
+
+    }, []);
+
+    return (
+        <main>
+            (items != null)? <div>
+            </div>
+
+        </main>
+        )
+}
